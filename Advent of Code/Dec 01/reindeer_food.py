38857b6cfa1e0da880713273ed2f423059f073a1
Grady <43967636+Rckymtnskier@users.noreply.github.com>
2022-12-01 13:16:10 +0000

    Added comments for clarity

--- Advent of Code/Dec 01/reindeer_food.py
+++ Advent of Code/Dec 01/reindeer_food.py
@@ -1,13 +1,17 @@
+#this references the local file to use as input and creates the requiste lists for the script
 file_to_use = '/workspaces/codespaces-jupyter/Advent of Code/Dec 01/data_input.txt'
 data_list = []
 elf_calories = []
 total_calories_per_elf = []
 
+#this opens the file and creates an iterable item of the contents
 with open(file_to_use, 'r') as file:
     contents = file.readlines()
 
+#this was for consistancy checking to see how many elves there were
 elf_count = contents.count('\n')
 
+#this converts the calorie strings to integers and then sorts them into nested lists within the main list by using '\n' as the delimiter
 for unit in contents:
     if unit != '\n':
         num = int(unit)
@@ -16,11 +20,14 @@ for unit in contents:
         data_list.append(elf_calories)
         elf_calories = []
 
+#this sums up the calories each elf is carrying
 for elf in data_list:
     elf_sum = sum(elf)
     total_calories_per_elf.append(elf_sum)
 
+#this finds the max value of calories being carried by an elf
 max_cal = max(total_calories_per_elf)
 
+#this prints the maximum amount of calories an elf is holding, and then finds which elf it is
 print(max_cal)
 print(total_calories_per_elf.index(max_cal))
