6aad81d515d95bc0bb7b6d2b0fc5df47edd9b874
Grady <43967636+Rckymtnskier@users.noreply.github.com>
2022-12-06 08:38:11 +0000

    Part 2 complete!

---
+++ Advent of Code/Dec 04 Part 2/overlap_pairs.py
@@ -0,0 +1,47 @@
+#function to find overlapping pairs
+def overlapping_pairs(pair_one, pair_two):
+    sp_one = len(pair_one) // 2
+    sp_two = len(pair_two) // 2
+    low_one = int(pair_one[:sp_one])
+    high_one = int(pair_one[sp_one:])
+    low_two = int(pair_two[:sp_two])
+    high_two = int(pair_two[sp_two:])
+
+    if low_one >= low_two and low_one <= high_two:
+        return 1
+    elif high_one >= low_two and high_one <= high_two:
+        return 1
+    elif low_two >= low_one and low_two <= high_one:
+        return 1
+    elif high_two >= low_one and high_two <= high_one:
+        return 1
+    else:
+        return 0
+
+
+#setting up variables
+file_to_use = '/workspaces/codespaces-jupyter/Advent of Code/Dec 04 Part 2/pairings.txt'
+pairing_list = []
+matched_list = []
+
+#read the pairings list
+with open(file_to_use, 'r') as file:
+    contents = file.readlines()
+
+#this is to strip down the strings and put them in a list
+for pair in contents:
+    x = pair.replace('\n', '')
+    x = x.replace('-', '')
+    y = x.split(',')
+    pairing_list.append(y)
+
+#this is a list comprehension to change the strings to integers
+#integer_pairs = [[int(group) for group in pair] for pair in pairing_list]
+
+#this is to run the function to find overlapping pairs
+for pair in pairing_list:
+    x = overlapping_pairs(pair[0], pair[1])
+    matched_list.append(x)
+
+#this is to find the total of overlapping pairs
+print(sum(matched_list))
