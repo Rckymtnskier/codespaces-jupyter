b1c558a8837a5883c443e17dcce19902e7380454
Grady <43967636+Rckymtnskier@users.noreply.github.com>
2022-12-06 13:56:29 +0000

    Part 1 complete!

--- Advent of Code/Dec 06 Part 1/start_of_packer_marker.py
+++ Advent of Code/Dec 06 Part 1/start_of_packer_marker.py
@@ -1,2 +1,24 @@
 #setting up variables
 file_to_use = '/workspaces/codespaces-jupyter/Advent of Code/Dec 06 Part 1/buffer_output.txt'
+header_check = ()
+i = 0
+
+#this is to read the data buffer into a variable
+with open(file_to_use, 'r') as file:
+    data_buffer = file.read()
+
+#this is to check the character against the buffer
+while i < len(data_buffer):
+    header_check = data_buffer[i:(i + 4)]
+    pos_one = header_check[0]
+    pos_two = header_check[1]
+    pos_three = header_check[2]
+    pos_four = header_check[3]
+    unique_check = header_check.count(pos_one) + header_check.count(pos_two) + header_check.count(pos_three) + header_check.count(pos_four)
+    if unique_check == 4:
+        print(i + 4)
+        break
+    else:
+        i += 1
+        header_check = ()
+    
